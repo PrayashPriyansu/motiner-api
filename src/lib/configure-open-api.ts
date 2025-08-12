303bad5efb4cfe98ae4ec38af2203ae13b00b2fc
Prayash Priyansu <prayashpriyansu27@gmail.com>
2025-08-12 12:48:13 +0530

    fix deployment error

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,13 +1,13 @@
 import type { AppOpenAPI } from "./types.js";
 
 import { Scalar } from "@scalar/hono-api-reference";
-import packageJson from "../../package.json";
+// import packageJson from '../../package.json' with { type: 'json' }
 
 export default function configureOpenApi(app: AppOpenAPI) {
   app.doc("/doc", {
     openapi: "3.0.0",
     info: {
-      version: packageJson.version,
+      version: "1.0.0",
       title: "Monitor API",
     },
   });
